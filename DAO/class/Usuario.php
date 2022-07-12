bc1a5139506b70c989124d3ee164e76595fbc344
masaccomani <maxandrey@gmail.com>
2022-07-12 09:30:55 -0300

    aulas

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -140,10 +140,30 @@ class Usuario {
 
     }
 
-    public function update(){
+    public function update($login, $password){
+
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
 
         $sql = new Sql();
-        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE id usuario = ")
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha(),
+            ':ID'=>$this->getIdusuario()
+        ));
+    }
+
+    public function delete(){
+
+        $sql = new Sql();
+        $sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
     }
 
     public function __construct($login = "", $password = ""){
@@ -152,6 +172,7 @@ class Usuario {
         $this->setDessenha($password);
     }
 
+    //"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
     public function __toString(){
 
         return json_encode(array(
@@ -159,8 +180,7 @@ class Usuario {
             "idusuario"=>$this->getIdusuario(),
             "deslogin"=>$this->getDeslogin(),
             "dessenha"=>$this->getDessenha(),
-            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-
+            "dtcadastro"=>$this->getDtcadastro()
         ));
 
     }
