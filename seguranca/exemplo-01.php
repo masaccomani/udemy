91b909dfaa8bd1373402f747b25db8d3be856983
masaccomani <maxandrey@gmail.com>
2022-11-18 18:56:54 -0300

    aulas

--- seguranca/exemplo-01.php
+++ seguranca/exemplo-01.php
@@ -1,5 +1,16 @@
 <?php
-	echo "<pre>";
-	$comando = system("dir C:", $retorno);
-	echo "</pre>";
+
+	if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+		$cmd = escapeshellcmd($_POST["cmd"]);
+		var_dump($cmd);
+
+		echo "<pre>";
+		$comando = system($cmd, $retorno);
+		echo "</pre>";
+	}
 ?>
+
+<form method="post">
+	<input type="text" name="cmd">
+	<button type="submit">Enviar</button>
+</form>
